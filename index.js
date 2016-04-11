71bb1b431d1f70135c2cc95a9ff59fc0ee9fcc78
藤本 真司 <fujimoto.shinji@classmethod.jp>
2016-04-11 12:52:08 +0900

    Initialize Kibana Plugin (generator-kibana-plugin v1.0.1)

---
+++ index.js
@@ -0,0 +1,30 @@
+import exampleRoute from './server/routes/example';
+
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+
+    uiExports: {
+      app: {
+        title: 'Devio',
+        description: 'An awesome Kibana plugin',
+        main: 'plugins/devio/app'
+      },
+      hacks: [
+        'plugins/devio/hack'
+      ]
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
+    init(server, options) {
+      // Add server routes and initalize the plugin here
+      exampleRoute(server);
+    }
+
+  });
+};
---
+++ public/app.js
@@ -0,0 +1,39 @@
+import moment from 'moment';
+import chrome from 'ui/chrome';
+import uiModules from 'ui/modules';
+import uiRoutes from 'ui/routes';
+
+import 'ui/autoload/styles';
+import './less/main.less';
+import template from './templates/index.html';
+
+chrome
+  .setNavBackground('#222222')
+  .setTabs([]);
+
+uiRoutes.enable();
+uiRoutes
+.when('/', {
+  template,
+  resolve: {
+    currentTime($http) {
+      return $http.get('../api/devio/example').then(function (resp) {
+        return resp.data.time;
+      });
+    }
+  }
+});
+
+uiModules
+.get('app/devio', [])
+.controller('devioHelloWorld', function ($scope, $route, $interval) {
+  $scope.title = 'Devio';
+  $scope.description = 'An awesome Kibana plugin';
+
+  var currentTime = moment($route.current.locals.currentTime);
+  $scope.currentTime = currentTime.format('HH:mm:ss');
+  var unsubscribe = $interval(function () {
+    $scope.currentTime = currentTime.add(1, 'second').format('HH:mm:ss');
+  }, 1000);
+  $scope.$watch('$destroy', unsubscribe);
+});
